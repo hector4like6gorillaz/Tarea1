82a281395bf37189b68ad98069eb8fe4ca5d2451
Rubensflotell <94159986+Rubensflotell@users.noreply.github.com>
2021-11-12 02:45:16 -0600

    tarea11

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("Hola Ruben");
+let PrimerNombre ="Ruben";
+
+let num1 = 25.5;
+let num2 = 234;
+let suma = num1 + num2
+
+PrimerNombre = "Ruben";
+
+console.log("el resultado de la suma de",num1 ," y ",num2, " es", suma);
